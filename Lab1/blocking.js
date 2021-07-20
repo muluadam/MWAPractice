bd6fd8f0fdd2c34698ddf1c087e2470019ec3b79
muluadam <muluadamt@gmail.com>
2021-07-20 07:18:36 -0500

    Blocking and Non blocking Febonaci

---
+++ Lab1/blocking.js
@@ -0,0 +1,4 @@
+console.log("1: Start");
+const feb=require('./fibonacci');
+
+console.log("2: Start");
--- Lab1/main.js
+++
@@ -1,5 +0,0 @@
-const feb=require('./fibonacci');
-
-console.log("Hellog World");
-console.log("Fibonacci of 30: "+feb.fibonacci(30));
-console.log("Fibonacci of -10: "+feb.fibonacci(-10));
